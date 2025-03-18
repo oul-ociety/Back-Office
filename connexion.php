59b21865e29cdd6c9e0e4d4e01f5d63d55c030a8
ousmane-diop <ousmanediop1110@gmail.com>
2025-03-18 16:56:03 +0000

    Creation des parametres et de la page de connexion de l'administrateur

--- connexion.php
+++ connexion.php
@@ -1,14 +1,14 @@
 <?php
-$host = 'crossover.proxy.rlwy.net';
-$dbname = 'railway';
-$username = 'root';
-$password = 'zExKSJjenIwGHTEAUgPwCcTLjfsFzkln';
-$port = '29638'; // Ajoutez le port si nécessaire
-
-try {
-    $pdo = new PDO("mysql:host=$host;port=$port;dbname=$dbname;charset=utf8", $username, $password);
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch (PDOException $e) {
-    die("Erreur de connexion : " . $e->getMessage());
-}
-?>
+ $host = 'crossover.proxy.rlwy.net';
+ $dbname = 'railway';
+ $username = 'root';
+ $password = 'zExKSJjenIwGHTEAUgPwCcTLjfsFzkln';
+ $port = '29638'; // Ajoutez le port si nécessaire
+ 
+ try {
+     $pdo = new PDO("mysql:host=$host;port=$port;dbname=$dbname;charset=utf8", $username, $password);
+     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+ } catch (PDOException $e) {
+     die("Erreur de connexion : " . $e->getMessage());
+ }
+ ?>
